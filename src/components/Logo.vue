58672e847cf6217a9cb83064dacd9eddd1ea6876
jyj <busy.joj@gmail.com>
2022-07-03 21:58:24 +0900

    kbs미디어 디지털 장비 관리 시스템 프로젝트

---
+++ src/components/Logo.vue
@@ -0,0 +1,27 @@
+<template>
+  <RouterLink
+    to="/"
+    class="logo">
+    <h1>
+      <img
+        src="../assets/img-logo-kbsmedia.png"
+        alt="KBS미디어" /><span class="blind">KBS미디어</span>
+    </h1>
+  </RouterLink>
+</template>
+
+<style lang="scss" scoped>
+@import "~/scss/main";
+.logo{
+    font-family: 'Oswald', sans-serif;
+    font-size: 20px;
+    color: $black;
+    text-decoration: none;
+    &:hover{
+        color:$black;
+    }
+    span{
+        color:$primary
+    }
+}
+</style>
