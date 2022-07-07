23c6f3d2f3ba61ba1dde6bc6898924f91fe85a27
busy-joj <busy.joj@gmail.com>
2022-07-07 18:06:39 +0900

    아이콘 이미지 출력 수정

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -5,9 +5,12 @@
         <button class="btn" type="button" aria-expanded="false" @click="dropdown()"></button>
         <ul class="dropdown-menu" :class="{'show' : isAddClass}">
           <li v-for="menu in infoDropdown" :key="menu.name">
+            <img :src="getImgUrl(menu.img)" alt="menu.img">
+            <!-- <img  :src="`~/assets/icon-info-${img}.png`" alt=""> -->
             <RouterLink
               :to="menu.href"
               class="dropdown-item">
+              
               {{ menu.name }}
             </RouterLink>
           </li>
@@ -23,17 +26,17 @@ export default{
                 infoDropdown:[
                     {
                         name : '내 정보', 
-                        // img : require('~/assets/icon/icon-info-my.png'),
+                        img : 'icon-info-my',
                         href : '/mypage'
                     },
                     {
                         name : '패스워드 변경',
-                        // img : require('~/assets/icon/icon-info-pw.png'),
+                        img : 'icon-info-pw',
                         href : '/movie'
                     },
                     {
                         name : '로그아웃',
-                        // img : require('~/assets/icon/icon-info-logout.png'),
+                        img : 'icon-info-logout',
                         href : '/about'
                     },
                 ],
@@ -43,6 +46,10 @@ export default{
         methods :{
           dropdown (){
             this.isAddClass = !this.isAddClass;
+          },
+          getImgUrl(pet){
+            var images = require.context('../assets/', false, /\.png$/)
+            return images('./' + pet + ".png")
           }
         }
     }
