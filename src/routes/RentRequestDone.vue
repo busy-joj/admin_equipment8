3593e61ca1d392ca32adb361ea851de14af1af22
ceedlive <ceedlive@naver.com>
2022-11-16 14:01:52 +0900

    ENH: #4 - FE/BE 연동을 위한 공통 작업 : 로그인 전후 화면 이동 제어 내비게이션 가드 추가
    - 라우팅 담당 index.js 파일에 beforeEnter 속성 추가 및 requireAuth 함수 추가
    
    - 라우팅 목록에 대여신청 완료 시 이동 화면 추가 및 route 화면 파일 생성 (RentRequestDone.vue)

---
+++ src/routes/RentRequestDone.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="sign-container"> 
+    <div class="inner">
+
+        <div class="succesed-msg">
+            <img src="~/assets/icon-b-check.png" alt="">
+            <p class="txt">대여신청이 완료되었습니다.<br />담당자 확인 후 신청한 기기를 찾아가주세요.</p>
+        </div>
+        
+        <div class="succesed-btns">
+          <BasicButton :class="{primary : true, lg: true, mn: true}" @click="onClickOk">확인</BasicButton>
+          <BasicButton :class="{white : true, lg: true, mn: true}" @click="onClickRequestHistory">신청 내역 조회하기</BasicButton>
+        </div>
+    </div><!-- /.inner -->
+  </div><!-- /.sign-container -->
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import BasicButton from '~/components/basic/BasicButton'
+
+export default {
+  components:{
+    BasicButton
+  },
+  computed:{
+    ...mapState('members',[
+      'succesedMsg'
+    ])
+  },
+  methods: {
+    onClickOk() {
+      this.$router.replace('/main')
+    },
+
+    onClickRequestHistory() {
+      console.log('신청 내역 조회하기')
+      this.$router.push({path: '/mypage', query: {tab: 'MyApplyList'}})
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.sign-container{
+  @include landscape{justify-content:center;padding-top:0px;}
+  .inner{text-align:center;
+    .succesed-msg{margin-bottom:80px;
+        img{width:120px;height:120px;}
+        .txt{font-size:24px;color:$M-gray5;margin-top:40px;
+        @include portrait{font-size:16px;margin-top:24px;}
+        }
+    }
+    .succesed-btns{
+      button + button{margin-top:16px;}
+      @include landscape{position:absolute;bottom:24px;left:16px;width:94%;}
+    }
+  }
+}
+</style>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 import Main from './Main'
 import Movie from './Movie'
 import About from './About'
@@ -10,43 +10,50 @@ import SignIn from './SignIn'
 import SignUp from './SignUp'
 import SignFind from './SignFind'
 import Success from './Success'
+import RentRequestDone from './RentRequestDone'
 // import SignUp from './SignUp'
 // import SignFind from './SignFind'
 
+function parseJwt (token) {
+    console.log('index.js token', token)
+    var base64Url = token.split('.')[1]
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/')
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
+    }).join(''))
+
+    return JSON.parse(jsonPayload)
+}
+
+// NavigationGuard
+const requireAuth = () => (from, to, next) => {
+    // const token = localStorage.getItem('user_token')
+    const accessToken = localStorage.getItem('access_token')
+
+    if (accessToken) {
+        const payload = parseJwt(accessToken)
+        if (payload.exp < Date.now() / 1000) { // 액세스토큰 만료
+            localStorage.removeItem('access_token')
+            next('/')
+        } else {  // 페이지 이동
+            return next()
+        }
+    } else {
+        next('/') // 로그인 화면으로 이동
+    }
+
+    // const isAuthenticated = false
+    // if (isAuthenticated) return next()
+    // next('/?returnPath=main')
+}
 
 export default createRouter({
     //Hash 모드
     history: createWebHashHistory(),
 
+    // history: createWebHistory(),
+
     routes: [
-        {
-            path: '/main',
-            component: Main
-        },
-        {
-            path: '/movie',
-            component: Movie
-        },
-        {
-            path: '/about',
-            component: About
-        },
-        {
-            path: '/account',
-            component: Account
-        },
-        {
-            path: '/mypage',
-            component: MyPage
-        },
-        {
-            path: '/rentApply',
-            component: RentApply
-        },
-        {
-            path: '/changepw',
-            component: ChangePw
-        },
         {
             path: '/',
             component: SignIn
@@ -60,8 +67,47 @@ export default createRouter({
             component : SignFind
         },
         {
+            path: '/main',
+            component: Main,
+            beforeEnter: requireAuth()
+        },
+        {
+            path: '/movie',
+            component: Movie
+        },
+        {
+            path: '/about',
+            component: About,
+            beforeEnter: requireAuth()
+        },
+        {
+            path: '/account',
+            component: Account,
+            beforeEnter: requireAuth()
+        },
+        {
+            path: '/mypage',
+            component: MyPage,
+            beforeEnter: requireAuth(),
+            props: true
+        },
+        {
+            path: '/rentApply',
+            component: RentApply
+        },
+        {
             path: '/success',
-            component : Success
-        }
+            component : Success,
+            beforeEnter: requireAuth()
+        },
+        {
+            path: '/rent/request/done',
+            component : RentRequestDone,
+            beforeEnter: requireAuth()
+        },
+        {
+            path: '/changepw',
+            component: ChangePw
+        },
     ]
 })
